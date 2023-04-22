5ce5a8a2420eac97415b53108ff4feec204027c1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-04-22 23:11:00 +0000

    main updates

--- main.cs
+++ main.cs
@@ -5,6 +5,7 @@ namespace HelloWorld {
     {
         public static void Main(string[] args){
            // Console.WriteLine("Comment's");
+           Console.WriteLine("Line 2")
         }
     }
 }
