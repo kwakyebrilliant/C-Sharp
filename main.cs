1b6a36a74c8ed47b86de33b9c1a5d162839bb290
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-04-22 23:15:10 +0000

    multiline comments

--- main.cs
+++ main.cs
@@ -4,9 +4,11 @@ namespace HelloWorld {
     class Program
     {
         public static void Main(string[] args){
+            /*
            Console.WriteLine("Comment's");
            Console.WriteLine("Line 2");
            Console.WriteLine("Line 3");
+           */
         }
     }
 }
