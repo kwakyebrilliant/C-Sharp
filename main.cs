7b581ff7b1587b7d28883c7637cbd2c2edf7b45e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-01 08:05:19 +0000

    main updates

--- main.cs
+++ main.cs
@@ -6,6 +6,7 @@ namespace HelloWorld {
         public static void Main(string[] args){
             //Data type variable = Data
             int val = 2,147,483,647
+            Console.WriteLine(int)
         }
     }
 }
