601ac4981e033153d2d257b6816714e4dd411e2e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-16 21:20:05 +0000

    switch added

--- main.cs
+++ main.cs
@@ -6,16 +6,12 @@ namespace HelloWorld {
         public static void Main(string[] args){
             //Conditions 
 
-            int age = 17;
+            char ch = 'A';
 
-            if(age>=18){
-                Console.WriteLine("You are eligible to vote")
-            }
-            else if(age < 18 && > 0) {
-                Console.WriteLine("You are not eligible to vote")
-            }
-            else{
-                Console.WriteLine("Invalid age")
+            switch (ch)
+            {
+                
+                default:
             }
 
         }
