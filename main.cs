beba746b6dc92f824a8c3b6117cf07bcfc7b4aeb
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-16 12:36:06 +0000

    main updates

--- main.cs
+++ main.cs
@@ -8,6 +8,9 @@ namespace HelloWorld {
 
             bool isRainning = false;
 
+            if(isRainning == true){
+                Console.WriteLine("Take your umbrella");
+            }
 
         }
     }
