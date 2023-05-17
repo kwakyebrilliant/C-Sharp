d6f17d647f1a0be23ade45cd04e07d247f77871e
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-17 21:38:16 +0000

    arrys

--- main.cs
+++ main.cs
@@ -4,16 +4,9 @@ namespace HelloWorld {
     class Program
     {
         public static void Main(string[] args){
-            //Loops 
+            //Arrays
 
-            int n = 2;
-            int i = 0;
-
-            while(i<=12){
-                int f = n * i;
-                Console.WriteLine(f);
-                i++;
-            }
+            
         }
     }
 }
