ff20a74a7948c1956b03098f56a324c2988770a0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-22 14:24:56 +0000

    updates

--- main.cs
+++ main.cs
@@ -7,10 +7,6 @@ namespace HelloWorld {
             //Arrays
 
             int[] val = {2, 1, 6, 3, 4};
-
-           for(int i = 0; i<val.length; i++){
-                Console.WriteLine(val[i]);
-           }
         }
     }
 }
