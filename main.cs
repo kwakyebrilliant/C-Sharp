1b61c75efdc103b7027c9d15aa2806b4f720a7ac
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-22 20:16:28 +0000

    main updates

--- main.cs
+++ main.cs
@@ -4,7 +4,13 @@ namespace HelloWorld {
     public class Program
     {
         public class Person(){
-            
+            public string name;
+            public int age;
+
+            public void GetName(){
+                
+            }
+
         }
 
         public static void Main(string[] args){
