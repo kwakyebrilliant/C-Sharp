16f589cd0a8175f201e7c6e003888b7190f4333c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-22 17:13:22 +0000

    classes

--- main.cs
+++ main.cs
@@ -4,13 +4,9 @@ namespace HelloWorld {
     class Program
     {
 
-        public static void _Hello(){
-            Console.WriteLine("Hello, world!")
-        }
-
         public static void Main(string[] args){
-            //Functions
-            _Hello();
+            //classes
+            
 
         }
     }
